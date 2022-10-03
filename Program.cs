534e563e2f18f1bcce86d74efa7214325012ee06
Den <workplacel@mail.ru>
2022-10-04 01:19:20 +0500

    finished sixth Task

--- Program.cs
+++ Program.cs
@@ -1,48 +1,17 @@
-﻿// Задача 4
-int a, b, c;
+﻿// Задача 6
+int a;
 string userEnter1;
-string userEnter2;
-string userEnter3;
 
 userEnter1 = Console.ReadLine();
-userEnter2 = Console.ReadLine();
-userEnter3 = Console.ReadLine();
 
 a = int.Parse(userEnter1);
-b = int.Parse(userEnter2);
-c = int.Parse(userEnter3);
 
 
-if(a > b)
+if(a % 2 == 1)
 {
-    if(b > c)
-    {
-        Console.Write($"max is {a}, and min is {c}");
-    }
-    else
-    {
-        Console.Write($"max is {a}, and min is {b}");
-    }
-}
-else if (b > c)
-{
-    if(c > a)
-    {
-        Console.Write($"max is {b}, and min is {a}");
-    }
-    else
-    {
-        Console.Write($"max is {b}, and min is {c}");
-    }
+    Console.Write($"{a} is odd");
 }
 else
 {
-    if(a > b)
-    {
-        Console.Write($"max is {c}, and min is {b}");
-    }
-    else
-    {
-        Console.Write($"max is {c}, and min is {a}");
-    }
+    Console.Write($"{a} is even");
 }
