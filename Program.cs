f3accb48fa87268bf86126844ed04090507c9ebd
Den <workplacel@mail.ru>
2022-10-04 00:46:51 +0500

    finished second Task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Задача 2
+int a, b;
+string userEnter1;
+string userEnter2;
+
+userEnter1 = Console.ReadLine();
+userEnter2 = Console.ReadLine();
+
+a = int.Parse(userEnter1);
+b = int.Parse(userEnter2);
+
+
+if(a > b)
+{
+    Console.Write($"max is {a}, and min is {b}");
+}
+else if (a == b)
+{
+    Console.Write($"There is no max and min number. {a} and {b} is equal");
+}
+else
+{
+    Console.Write($"max is {b}, and min is {a}");
+}
