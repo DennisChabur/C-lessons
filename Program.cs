6aa4633b2174c6b04a75c7b27174bb84b4565980
Den <workplacel@mail.ru>
2022-10-04 01:13:31 +0500

    finished fourth Task

--- Program.cs
+++ Program.cs
@@ -1,24 +1,48 @@
-﻿// Задача 2
-int a, b;
+﻿// Задача 4
+int a, b, c;
 string userEnter1;
 string userEnter2;
+string userEnter3;
 
 userEnter1 = Console.ReadLine();
 userEnter2 = Console.ReadLine();
+userEnter3 = Console.ReadLine();
 
 a = int.Parse(userEnter1);
 b = int.Parse(userEnter2);
+c = int.Parse(userEnter3);
 
 
 if(a > b)
 {
-    Console.Write($"max is {a}, and min is {b}");
+    if(b > c)
+    {
+        Console.Write($"max is {a}, and min is {c}");
+    }
+    else
+    {
+        Console.Write($"max is {a}, and min is {b}");
+    }
 }
-else if (a == b)
+else if (b > c)
 {
-    Console.Write($"There is no max and min number. {a} and {b} is equal");
+    if(c > a)
+    {
+        Console.Write($"max is {b}, and min is {a}");
+    }
+    else
+    {
+        Console.Write($"max is {b}, and min is {c}");
+    }
 }
 else
 {
-    Console.Write($"max is {b}, and min is {a}");
+    if(a > b)
+    {
+        Console.Write($"max is {c}, and min is {b}");
+    }
+    else
+    {
+        Console.Write($"max is {c}, and min is {a}");
+    }
 }
